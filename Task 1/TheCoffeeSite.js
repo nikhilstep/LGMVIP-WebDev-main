d96fd378db76eb7b073ca9cd7724f0b7dd1a5042
NIKHIL STEPHEN RAJ B C <61453559+nikhilstep@users.noreply.github.com>
2021-11-21 19:31:41 +0530

    Add files via upload

---
+++ Task 1/TheCoffeeSite.js
@@ -0,0 +1,11 @@
+function mo(){
+
+    var ic = document.getElementsByClassName("image1");
+    var a = document.getElementsByClassName("a1");
+    ic.addEventListener("onmouseover",()=>{
+        a.style.opacity="1";
+    })
+    ic.addEventListener("onclick",()=>{
+        a.style.opacity="1";
+    })
+}
